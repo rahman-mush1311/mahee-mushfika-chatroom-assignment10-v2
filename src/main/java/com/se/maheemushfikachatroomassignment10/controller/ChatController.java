f17c5be8d2237bb649b0c035ed290dc9eb226e76
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-10-31 16:18:01 -0500

    get all user api added

--- src/main/java/com/se/maheemushfikachatroomassignment10/controller/ChatController.java
+++ src/main/java/com/se/maheemushfikachatroomassignment10/controller/ChatController.java
@@ -1,23 +1,38 @@
 package com.se.maheemushfikachatroomassignment10.controller;
 
+import com.se.maheemushfikachatroomassignment10.model.ChatUser;
 import com.se.maheemushfikachatroomassignment10.model.Message;
+import com.se.maheemushfikachatroomassignment10.repository.ChatUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
 @Controller
 public class ChatController {
 
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
 
+    @Autowired
+    private ChatUserRepository chatUserRepository;
+
     @MessageMapping("/message")
     @SendTo("/chatroom/public")
     public Message receiveMessage(@Payload Message message) {
         return message;
     }
+
+    @GetMapping(value = "/users")
+    public ResponseEntity<List<ChatUser>> getUsers() {
+        return new ResponseEntity<>(chatUserRepository.findAll(), HttpStatus.OK);
+    }
+
 }
