49d35390103d61177dd2f5bca6343b7a8c1ca1f9
rahman-mush1311 <tuktuki1311@yahoo.com>
2022-10-31 15:57:11 -0500

    message mapping added

---
+++ src/main/java/com/se/maheemushfikachatroomassignment10/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.se.maheemushfikachatroomassignment10.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/message")
+    @SendTo("/chatroom/public")
+    public Message receiveMessage(@Payload Message message) {
+        return message;
+    }
+}
